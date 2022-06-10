09a603985b80187450d5eda9bb5f33d9694d81ae
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-06-10 16:17:54 -0500

    changed prompts into alerts at end of games

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let answer  = window.prompt('Do you head left or right?');
+let answer = window.prompt ('Do you head left or right?');
 
-if(Answer === 'left'){
+if (Answer === 'left'){
     Answer = window.prompt (`You come across a stray cat. It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
 }
@@ -11,7 +11,7 @@ Do you follow it, or continue on your path?`)
             answer = window.prompt ('You live happily amongst the cats for the rest of your days.')
             }  
         else (answer === 'spread'){
-                answer = window.prompt ('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
+                answer = window.alert ('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
             }
             
     else if (answer === 'continue'){
@@ -21,32 +21,30 @@ Do you follow it, or continue on your path?`)
             answer = window.prompt ('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.')
         }
         else (answer === 'staircase'){
-            answer = window.prompt ('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.')
+            answer = window.alert ('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.')
         }
           
 
 
 else if(answer === 'right') {
-    Answer = window.prompt(`You come across a snoring dragon. 
-On the other side of him, you see a shiny chest of treasure. Another path would 
-lead you away from the dragon altogether. Which path do you take?`)
+    Answer = window.prompt(`You come across a snoring dragon. On the other side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon altogether. Which path do you take?`)
 }
     if (answer === 'past the dragon'){
         answer = window.prompt ('The dragon wakes up and sits upright. You only have a moment to respond, to stay or run:')
     }
         if (answer === 'stay') {
-            answer = window prompt ('You and the dragon have an uplifting conversation about local politics and become lifelong friends.')
+            answer = window alert ('You and the dragon have an uplifting conversation about local politics and become lifelong friends.')
     }
         else (answer === 'run') {
-            answer = window.prompt ("Quickly, you run back to the cave's entrance. Sheepish, you return home.")
+            answer = window.alert ("Quickly, you run back to the cave's entrance. Sheepish, you return home.")
         }
 
     else if (answer === 'away from the dragon'){
         answer = window.prompt ('After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?')
     }
         if (answer === 'draw it'){
-            answer = window.prompt ('You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch.')
+            answer = window.alert ('You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch.')
         }
         else (answer === 'pick it'){
-            answer = window.prompt ('You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.')
+            answer = window.alert ('You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.')
         }
